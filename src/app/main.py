f8e4c7fda04076b6624b34b85598ea3d5792fda4
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-08 19:54:14 +0000

    Add main.py with FastAPI implementation

---
+++ src/app/main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import boto3
+import json
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+import uvicorn
+
+# boto_session = boto3.Session()
+# credentials = boto_session.get_credentials()
+
+# bedrock_models = boto3.client('bedrock')
+
+# MODEL_ID = 'amazon.titan-embed-g1-text-v1'
+
+# Create FastAPI instance
+app = FastAPI()
+
+# Define Pydantic model
+class Item(BaseModel):
+    text: str
+    
+@app.get('/')
+def root():
+    return Response("<h1>API to Question and Answering</h1>")
+    
+# Define API route
+@app.post("/predict")
+def predict(item: Item):
+    return {"text": item.text}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
+    
