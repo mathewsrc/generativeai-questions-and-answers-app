c9cade03eae801f2c7efb247456fb24812576f84
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-10 17:32:08 +0000

    Add script to install Terraform

---
+++ install_terraform.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Terraform by HashiCorp
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Add the official HashiCorp Linux repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update and install
+sudo apt update && sudo apt install terraform
+
+# Verify the installation
+terraform --version
