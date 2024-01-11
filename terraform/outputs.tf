535bada1b2f3839a58000046f7c82e3731e25a66
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-11 18:44:49 +0000

    Update S3 object URLs in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,5 @@ output "bedrock_role" {
 }
 
 output "s3_object_urls" {
-  value = [for obj in aws_s3_object.pdf : aws_s3_object.pdf[obj].source]
+  value = [for obj in aws_s3_object.object : aws_s3_object.object[obj].source]
 }
