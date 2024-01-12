689b32dc4fd9e14168f476123753b1afd060d423
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-12 01:17:58 +0000

    Add global variables for vectorstore, documents, AWS region, embedding model, S3 bucket, and LLM model

---
+++ src/cli/global_variables.py
@@ -0,0 +1,6 @@
+VECTORSTORE_PATH = "./vectorstore/vectorstore.joblib"
+DOCUMENTS_PATH = "./documents"
+AWS_REGION = "us-east-1"
+EMBEDDING_MODEL = "amazon.titan-embed-text-v1"
+AWS_S3_BUCKET = "bedrock-question-answer"
+BEDROCK_TEXT_MODEL = "anthropic.claude-v2"
