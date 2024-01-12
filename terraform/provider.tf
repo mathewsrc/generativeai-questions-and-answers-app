e3a0a7f2dce784bf882ad4ac7f481c435798b2a3
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-12 20:42:07 +0000

    Remove unnecessary comment

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,7 +16,6 @@ provider "aws" {
   }
 }
 
-## This is for demo porpuses, for production use, use a remote state backed by Amazon S3
 terraform {
   backend "local" {
     path = "terraform.tfstate"
