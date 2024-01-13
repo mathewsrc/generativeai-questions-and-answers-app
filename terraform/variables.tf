bec5c5dcf36a4c3093aefbdfd61da08293bc28e6
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-13 20:01:38 +0000

    Update variable descriptions and names in terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,16 @@
-# General variables
 variable "region" {
   default     = "us-east-1"
-  description = "The region you want to deploy the solution"
+  description = "The region of deploy"
 }
 
 variable "name" {
   default     = "rag"
-  description = "the name for resource"
+  description = "The name for resource"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket"
-  default     = "bedrock-question-answer"
+  default     = "bedrock-question-answer-tf"
 }
 
 variable "environment" {
@@ -21,30 +20,46 @@ variable "environment" {
 
 variable "ecr_name" {
   description = "The name of ECR repository"
-  default     = "bedrock/bedrock_qa"
+  default     = "bedrock/bedrock-qa-tf"
 }
 
 variable "ecs_task_name" {
   description = "The ECS task definition name"
-  default     = "bedrock-qa-task"
+  default     = "bedrock-qa-task-tf"
 }
 
 variable "ecs_cluster_name" {
   description = "The name of the ECS cluster"
-  default     = "bedrock-qa-cluster"
+  default     = "bedrock-qa-cluster-tf"
 }
 
 variable "ecs_service_name" {
   description = "Name of the ECS service"
-  default     = "bedrock-qa-service"
+  default     = "bedrock-qa-service-tf"
 }
 
 variable "ecs_execution_role_name" {
-  description = "The name of ecs execution role"
-  default     = "ecs_execution_role"
+  description = "The name of ECS execution role"
+  default     = "ecs-execution-role_tf"
 }
+
+variable "ecs_security_group_name" {
+  description = "The name of the ECS security group"
+  default     = "ecs-security-group-tf"
+}
+
+variable "load_balance_name" {
+  description = "The name of Load Balance"
+  default     = "load-balance-tf"
+}
+
+variable "load_balancer_target_group_name" {
+  description = "The name of Load balancer target group"
+  default     = "target-group-tf"
+}
+
 
 #variable "collection_name" {
 #  description = "Name of the OpenSearch Serverless collection."
-#  default     = "bedrock-qa-collection"
+#  default     = "bedrock-qa-collection-tf"
 #}
