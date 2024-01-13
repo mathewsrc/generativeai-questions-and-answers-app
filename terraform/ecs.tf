dd379f0a54c884ecd748646f7798bc3d25d0dff8
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-13 18:29:58 +0000

    Add ECS cluster and task definition resources

---
+++ terraform/ecs.tf
@@ -0,0 +1,58 @@
+resource "aws_ecs_cluster" "my_ecs_cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family = var.ecs_task_name
+
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "bedrock-api",
+      "image": "${aws_ecr_repository.bedrock.repository_url}:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8000,
+          "hostPort": 8000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+
+  runtime_platform {
+    operating_system_family = "Linux"
+    cpu_architecture        = "X86_64"
+  }
+
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
