2f55d2aa9f7550577c6535195260169ac6b315f7
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-13 21:37:03 +0000

    Add remote backend configuration for Terraform state storage

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -21,3 +21,15 @@ terraform {
     path = "terraform.tfstate"
   }
 }
+
+#terraform {
+# backend "remote" {
+#    # The name of your Terraform Cloud organization.
+#    organization = "mlops-terraform"
+#
+#    # The name of the Terraform Cloud workspace to store Terraform state files in.
+#    workspaces {
+#      name = "bedrock-qa-tf"
+#    }
+#  }
+#}
