6a3b809af59de94d56df4b049b695d0d4b76c6db
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-14 18:04:17 +0000

    Add tags to load balancer resources

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -13,6 +13,12 @@ resource "aws_security_group" "ecs_load_balancer_security_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Environment = var.environment
+    Application = var.name
+    Name        = var.ecs_security_group_name
+  }
 }
 
 resource "aws_lb" "ecs_load_balancer" {
@@ -28,7 +34,9 @@ resource "aws_lb" "ecs_load_balancer" {
   enable_deletion_protection = false
 
   tags = {
-    Environment = "production"
+    Environment = var.environment
+    Application = var.name
+    Name        = var.load_balance_name
   }
 }
 
@@ -37,6 +45,12 @@ resource "aws_lb_target_group" "lb_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_default_vpc.default_vpc.id
+
+  tags = {
+    Environment = var.environment
+    Application = var.name
+    Name        = var.load_balancer_target_group_name
+  }
 }
 
 resource "aws_lb_listener" "lb_listener" {
@@ -46,5 +60,10 @@ resource "aws_lb_listener" "lb_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.lb_target_group.arn #
+  }
+
+  tags = {
+    Environment = var.environment
+    Application = var.name
   }
 }
