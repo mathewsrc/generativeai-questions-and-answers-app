4382dfe7005013207e17fd23ee260342b5d6ba72
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-14 18:03:55 +0000

    Update ECR repository configuration

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,12 +1,14 @@
-#data "aws_ecr_repository" "bedrock" {
-# name = var.ecr_name
-#}
-
 resource "aws_ecr_repository" "bedrock" {
-  name                 = "bedrock_qa"
+  name                 = var.ecr_name
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  tags = {
+    Environment = var.environment
+    Application = var.name
+    Name        = var.ecr_name
   }
 }
