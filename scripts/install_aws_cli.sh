1bd0821db9ff9ccc211b27b46932dc308db8f559
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-17 16:31:12 +0000

    Add comments to scripts

--- scripts/install_aws_cli.sh
+++ scripts/install_aws_cli.sh
@@ -2,9 +2,20 @@
 
 # This script is used to install the AWS CLI
 
+# Install the AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Unzip the AWS CLI
 unzip awscliv2.zip
+
+# Install the AWS CLI
 sudo ./aws/install
+
+# Clean up the files
 rm -rf awscliv2.zip aws
+
+# Verify the AWS CLI
 aws --version
+
+# Configure the AWS CLI
 aws configure
--- scripts/install_poetry.sh
+++ scripts/install_poetry.sh
@@ -2,8 +2,17 @@
 
 # This script is used to install Poetry
 
-pipx install --force poetry 
-poetry completions bash >> ~/.bash_completion 
+# Install Poetry
+pipx install --force poetry
+
+# Enable tab completion for Bash
+poetry completions bash >> ~/.bash_completion
+
+# Init Poetry
 poetry init
+
+# Install Poetry dependencies
 poetry install
+
+# Check Poetry version
 poetry --version
