0d3c8f97c2e474b35bf81e592ffeb3c6c9235e00
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-17 19:12:12 +0000

    Add output for git SHA

--- terraform/ecs/outputs.tf
+++ terraform/ecs/outputs.tf
@@ -6,3 +6,7 @@ output "ecs_service_name" {
   value = aws_ecs_service.ecs_service.name
 }
 
+output "git_sha" {
+  description = "The latest git commit SHA"
+  value       = data.external.envs.result.sha
+}
