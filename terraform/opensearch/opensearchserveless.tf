a293d10e9cda0618785507f543cfb556dd13b511
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-19 22:48:36 +0000

    Add OpenSearch Serverless configuration

---
+++ terraform/opensearch/opensearchserveless.tf
@@ -0,0 +1,61 @@
+# Get the current AWS account ID
+data "aws_caller_identity" "current" {}
+
+# Creates an encryption security policy
+resource "aws_opensearchserverless_security_policy" "encryption_policy" {
+  name        = var.encryption_policy_name
+  type        = var.security_policy_type
+  description = "encryption policy for ${var.collection_name}"
+  policy = jsonencode({
+    Rules = [
+      {
+        Resource = [
+          "collection/${var.collection_name}"
+        ],
+        ResourceType = "collection"
+      }
+    ],
+    AWSOwnedKey = true
+  })
+}
+
+# Creates a collection
+resource "aws_opensearchserverless_collection" "collection" {
+  name       = var.collection_name
+  type       = var.collection_type
+  depends_on = [aws_opensearchserverless_security_policy.encryption_policy]
+}
+
+# Creates a data access policy
+resource "aws_opensearchserverless_access_policy" "data_access_policy" {
+  name        = var.data_access_policy_name
+  type        = var.data_acess_policy_type
+  description = "allow index and collection access"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "index",
+          Resource = [
+            "index/${var.collection_name}/*"
+          ],
+          Permission = [
+            "aoss:*"
+          ]
+        },
+        {
+          ResourceType = "collection",
+          Resource = [
+            "collection/${var.collection_name}"
+          ],
+          Permission = [
+            "aoss:*"
+          ]
+        }
+      ],
+      Principal = [
+        data.aws_caller_identity.current.arn
+      ]
+    }
+  ])
+}
