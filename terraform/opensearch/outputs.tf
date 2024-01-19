d6a72ea342e077b2e48a91d8bee54cb4b5262a8d
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-19 22:48:46 +0000

    Update outputs.tf with collection and dashboard endpoints

--- terraform/opensearch/outputs.tf
+++ terraform/opensearch/outputs.tf
@@ -1,15 +1,8 @@
-output "bedrock_role" {
-  value = aws_iam_role.bedrock.arn
+
+output "collection_enpdoint" {
+  value = aws_opensearchserverless_collection.collection.collection_endpoint
 }
 
-#output "collection_enpdoint" {
-#  value = aws_opensearchserverless_collection.collection.collection_endpoint
-#}
-
-#output "dashboard_endpoint" {
-# value = aws_opensearchserverless_collection.collection.dashboard_endpoint
-#}
-
-#output "ecr_repository" {
-# value = aws_ecr_repository.bedrock.arn
-#}
+output "dashboard_endpoint" {
+  value = aws_opensearchserverless_collection.collection.dashboard_endpoint
+}
