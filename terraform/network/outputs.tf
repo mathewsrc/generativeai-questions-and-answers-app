31236467fc8003a45c5c5ff9573e4f190da6307b
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-19 22:49:01 +0000

    Update load balancer and security group names in network outputs

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -1,13 +1,17 @@
 output "load_balancer_name" {
-  value = aws_lb.ecs_load_balancer.name
+  value = aws_lb.load_balancer.name
 }
 
 output "target_group_arn" {
   value = aws_lb_target_group.lb_target_group.arn
 }
 
-output "ecs_service_security_groups_id" {
-  value = tolist([aws_security_group.ecs_load_balancer_security_group.id])
+output "service_security_group_ids" {
+  value = tolist([aws_security_group.service_security_group.id])
+}
+
+output "vpc_id" {
+  value = aws_default_vpc.default_vpc.id
 }
 
 output "subnets" {
@@ -15,5 +19,5 @@ output "subnets" {
 }
 
 output "app_url" {
-  value = aws_lb.ecs_load_balancer.dns_name
+  value = aws_lb.load_balancer.dns_name
 }
