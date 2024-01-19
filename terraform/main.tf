51dc1ec44a94c3e27e369406c94c82644790dcfb
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-01-19 22:49:18 +0000

    Remove commented out S3 modules and add OpenSearch module

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,22 +30,32 @@ module "network" {
   environment = var.environment
 }
 
-module "s3_cnu" {
-  source      = "./s3"
-  region      = var.region
-  name        = var.name
-  bucket_name = "bedrock-qa-bucket-tf"
-  environment = var.environment
-  subfolder   = "cnu"
-}
+#module "s3_cnu" {
+#  source      = "./s3"
+#  region      = var.region
+#  name        = var.name
+#  bucket_name = "bedrock-qa-bucket-tf"
+#  environment = var.environment
+#  subfolder   = "cnu"
+#}
 
-module "s3_immigration" {
-  source      = "./s3"
-  region      = var.region
-  name        = var.name
-  bucket_name = "bedrock-qa-bucket-tf"
-  environment = var.environment
-  subfolder   = "immigration"
+#module "s3_immigration" {
+#  source      = "./s3"
+#  region      = var.region
+#  name        = var.name
+#  bucket_name = "bedrock-qa-bucket-tf"
+#  environment = var.environment
+#  subfolder   = "immigration"
+#}
+
+module "opensearchserveless" {
+  source             = "./opensearch"
+  region             = var.region
+  name               = var.name
+  environment        = var.environment
+  subnet_ids         = module.network.subnets
+  vpc_id             = module.network.vpc_id
+  security_group_ids = module.network.service_security_group_ids
 }
 
 module "ecr" {
@@ -56,16 +66,16 @@ module "ecr" {
 }
 
 module "ecs" {
-  source                         = "./ecs"
-  region                         = var.region
-  name                           = var.name
-  environment                    = var.environment
-  ecr_repository_url             = module.ecr.ecr_repository_url
-  ecr_repository_name            = module.ecr.ecr_repository_name
-  target_group_arn               = module.network.target_group_arn
-  subnets                        = module.network.subnets
-  ecs_service_security_groups_id = module.network.ecs_service_security_groups_id
-  ecs_task_execution_role_arn    = module.iam.ecs_task_execution_role_arn
+  source                      = "./ecs"
+  region                      = var.region
+  name                        = var.name
+  environment                 = var.environment
+  ecr_repository_url          = module.ecr.ecr_repository_url
+  ecr_repository_name         = module.ecr.ecr_repository_name
+  target_group_arn            = module.network.target_group_arn
+  subnets                     = module.network.subnets
+  ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
+  service_security_group_ids  = module.network.service_security_group_ids
 }
 
 # terraform {
@@ -84,3 +94,4 @@ terraform {
     }
   }
 }
+
