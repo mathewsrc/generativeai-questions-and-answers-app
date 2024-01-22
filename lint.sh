5be96ec09badc78f1b38657a81d2d44cc216ce6b
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-22 15:33:53 -0300

    Update ruff lint command and add --fix option

--- lint.sh
+++ lint.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 for DIR in */; do
     DIRNAME=$(basename "$DIR")
     echo "==> $DIRNAME <=="
-    (cd $DIR && poetry run ruff check *.py)
+    (cd $DIR && poetry run ruff check . --fix)
 done
 
 echo "Format complete."
