5dfae1ace4ee556f8a0d3c4f78c6ebf1af8067b3
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-22 15:32:57 -0300

    Update ruff command

--- format.sh
+++ format.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 for DIR in */; do
     DIRNAME=$(basename "$DIR")
     echo "==> $DIRNAME <=="
-    (cd $DIR && poetry run ruff format *.py)
+    (cd $DIR && poetry run ruff format .)
 done
 
 echo "Format complete."
