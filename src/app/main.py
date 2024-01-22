6b08bfbf89bbb98e0dc1b125811e8d3bc699c9f8
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-22 15:38:25 -0300

    Refactor main.py

--- src/app/main.py
+++ src/app/main.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import boto3
-import joblib
 from langchain.llms import Bedrock
 
+
 class Question(BaseModel):
-    content: str
+	content: str
+
 
 app = FastAPI()
 
@@ -18,21 +19,25 @@ credentials = boto_session.get_credentials()
 bedrock_models = boto3.client("bedrock", region_name=AWS_REGION)
 bedrock_runtime = boto3.client("bedrock-runtime", region_name=AWS_REGION)
 
+
 @app.get("/", response_class=HTMLResponse)
 async def root():
-    return HTMLResponse("""
+	return HTMLResponse(
+		"""
     <h1>Welcome to our Question/Answering application with Bedrock</h1>
     <p>Use the /question endpoint to ask a question.</p>
-    """)
+    """
+	)
+
 
 @app.get("/question", response_class=HTMLResponse)
 async def question(question: Question):
-    llm = Bedrock(
-        client=bedrock_runtime,
-        model_id="anthropic.claude-v2",
-    )
-    try:
-        answer = "Hello from FastAPI"
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    return HTMLResponse(f"<h1>Question: {question.content}</h1><p>Answer: {answer}</p>")
+	llm = Bedrock(
+		client=bedrock_runtime,
+		model_id="anthropic.claude-v2",
+	)
+	try:
+		answer = "Hello from FastAPI"
+	except Exception as e:
+		raise HTTPException(status_code=500, detail=str(e))
+	return HTMLResponse(f"<h1>Question: {question.content}</h1><p>Answer: {answer}</p>")
