d04c284b15d94d86e31e77aefa72ca91dd5635ae
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-22 15:39:43 -0300

    Update global variables in CLI module

--- src/cli/global_variables.py
+++ src/cli/global_variables.py
@@ -1,7 +1,4 @@
-VECTORSTORE_PATH = "./vectorstore/vectorstore.joblib"
 DOCUMENTS_PATH = "./documents"
 AWS_REGION = "us-east-1"
-EMBEDDING_MODEL = "amazon.titan-embed-text-v1"
 AWS_S3_BUCKET = "bedrock-question-answer"
-BEDROCK_TEXT_MODEL = "anthropic.claude-v2"
-COLLECTION_NAME = "question_answering"
+COLLECTION_NAME = "cnu"
