95b3d2201c4a9c90ea1bfae0faed4663551f78c4
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-23 13:27:22 -0300

    Fix typo in load_balancer_arn output

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -2,8 +2,12 @@ output "load_balancer_name" {
   value = aws_lb.load_balancer.name
 }
 
-output "laod_balancer_arn" {
-  value = aws_lb.load_balancer.arn  
+output "load_balancer_arn" {
+  value = aws_lb.load_balancer.arn
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.load_balancer.dns_name
 }
 
 output "target_group_arn" {
