e79bc6378a77b493fb61d862eaf1b5c82ca9011a
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-23 13:10:07 -0300

    Update variables.tf with load balancer and API configuration

--- terraform/api_gateway/variables.tf
+++ terraform/api_gateway/variables.tf
@@ -1,5 +1,10 @@
-variable "laod_balancer_arn"{
+variable "load_balancer_arn"{
     description = "The ARN of the load balancer"
+    type = string
+}
+
+variable "load_balancer_dns_name"{
+    description = "The DNS name of the load balancer"
     type = string
 }
 
@@ -7,6 +12,60 @@ variable "vpc_link_name" {
     description = "The name of the VPC link"
     default = "vpc-link"
     type = string
+}
+
+variable "api_name"{
+    description = "The name of the API"
+    default = "bedrock-qa-api"
+    type = string
+}
+
+variable "api_stage_name"{
+    description = "The name of the API stage"
+    default = "dev"
+    type = string
+}
+
+variable "logs_retantion_in_days"{
+    description = "The number of days to retain the logs"
+    default = 7
+    type = number
+}
+
+variable "usage_plan_name"{
+    description = "The name of the usage plan"
+    default = "qa-usage-plan"
+    type = string
+}
+
+variable "period"{
+    description = "The period of the usage plan"
+    default = "WEEK"
+    type = string
+}
+
+variable "quota_limit"{
+    description = "Maximum number of requests that can be made in a given time period."
+    default = 20
+    type = number
+}
+
+variable "quota_offset"{
+    description = "Number of requests to subtract from the given limit.  "
+    default = 2
+    type = number
+}
+
+variable "burst_limit" {
+    description = "The maximum rate limit over a time ranging from one to a few seconds"
+    default = 5
+    type = number
+}
+
+variable "rate_limit" {
+    description = "The API request steady-state rate limit."
+    default = 10
+    type = number
 }
 
 variable "name" {
