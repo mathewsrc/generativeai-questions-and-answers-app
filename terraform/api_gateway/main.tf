0c6e72ef2e6aa327f9d0762715d74825fdb1fe8f
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-23 21:38:51 -0300

    Update API Gateway integration URIs

--- terraform/api_gateway/main.tf
+++ terraform/api_gateway/main.tf
@@ -1,7 +1,8 @@
+# Create a VPC Link from the API Gateway to the NLB
 resource "aws_api_gateway_vpc_link" "vpc_link" {
   name        = var.vpc_link_name
-  description = "VPC Link for API Gateway"
-  target_arns = [var.load_balancer_arn]
+  description = "VPC link for API Gateway"
+  target_arns = [var.nlb_arn]
   tags = {
     Name        = var.name
     Environment = var.environment
@@ -55,32 +56,27 @@ resource "aws_api_gateway_integration" "root_get_integration" {
 
   type                    = "HTTP_PROXY"
   integration_http_method = "GET"
-  uri                     = "http://${var.load_balancer_dns_name}/"
+  uri                     = "http://${var.nlb_dns_name}/:${var.container_port}/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.vpc_link.id
 }
 
 # Integration for POST /ask
 resource "aws_api_gateway_integration" "ask_post_integration" {
+
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.ask_resource.id
   http_method = aws_api_gateway_method.ask_post.http_method
 
   type                    = "HTTP_PROXY"
   integration_http_method = "POST"
-  uri                     = "http://${var.load_balancer_dns_name}/ask"
+  uri                     = "http://${var.nlb_dns_name}/:${var.container_port}/ask"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.vpc_link.id
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.root_get_integration,
-    aws_api_gateway_integration.ask_post_integration
-  ]
-
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = var.api_stage_name
   description = "Deployment for the dev stage"
 
   lifecycle {
@@ -88,47 +84,36 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   }
 }
 
-resource "aws_api_gateway_stage" "production" {
+resource "aws_api_gateway_stage" "api_stage" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
   stage_name    = var.api_stage_name
-  description   = "Stage for the dev environment"
 }
 
-resource "aws_api_gateway_method_settings" "all" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = aws_api_gateway_stage.production.stage_name
-  method_path = "*/*" # This is the method path for all methods in the API
+# resource "aws_api_gateway_method_settings" "all" {
+#   rest_api_id = aws_api_gateway_rest_api.api.id
+#   stage_name  = aws_api_gateway_stage.api_stage.stage_name
+#   method_path = "*/*" # This is the method path for all methods in the API
 
-  settings {
-    metrics_enabled = true
-    logging_level   = "INFO"
-  }
-}
+#   settings {
+#     metrics_enabled = true
+#     logging_level   = "INFO"
+#   }
+# }
 
-resource "aws_cloudwatch_log_group" "log_group" {
-  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.api.id}/${var.api_stage_name}"
-  retention_in_days = var.logs_retantion_in_days
+# resource "aws_cloudwatch_log_group" "log_group" {
+#   name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.api.id}/${var.api_stage_name}"
+#   retention_in_days = var.logs_retantion_in_days
 
-  tags = {
-    Name        = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.api.id}/${var.api_stage_name}"
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Name        = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.api.id}/${var.api_stage_name}"
+#     Environment = var.environment
+#   }
+# }
 
 resource "aws_api_gateway_usage_plan" "usage_plan" {
   name        = var.usage_plan_name
   description = "QA Usage Plan"
-
-#   api_stages {
-#     api_id = aws_api_gateway_rest_api.api.id
-#     stage  = aws_api_gateway_stage.development.stage_name
-#   }
-
-  api_stages {
-    api_id = aws_api_gateway_rest_api.api.id
-    stage  = aws_api_gateway_stage.production.stage_name
-  }
 
   quota_settings {
     limit  = var.quota_limit  # Maximum number of requests that can be made in a given time period.
@@ -137,8 +122,8 @@ resource "aws_api_gateway_usage_plan" "usage_plan" {
   }
 
   throttle_settings {
-    burst_limit = 5  # The maximum rate limit over a time ranging from one to a few seconds
-    rate_limit  = 10 # The API request steady-state rate limit.
+    burst_limit = var.burst_limit # The maximum rate limit over a time ranging from one to a few seconds
+    rate_limit  = var.rate_limit  # The API request steady-state rate limit.
   }
 
   tags = {
