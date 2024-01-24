332d4fdabe2d0056f89efd0590be8d0603e85f7b
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-23 21:37:39 -0300

    Add VPC endpoints for ECR, ECR API, CloudWatch, and S3

---
+++ terraform/network/vpc_endpoints.tf
@@ -0,0 +1,68 @@
+data "aws_region" "current" {} 
+
+# Create a VPC for ECR 
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id       = "${aws_vpc.main.id}"
+  service_name = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = aws_subnet.private_subnets.*.id
+
+  security_group_ids = [
+    aws_security_group.ecs_tasks.id,
+  ]
+
+  tags = {
+    Name = "ECR Docker VPC Endpoint - ${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# Create a VPC Endpoint for ECR API
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id       = "${aws_vpc.main.id}"
+  service_name = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = aws_subnet.private_subnets.*.id
+
+  security_group_ids = [
+    aws_security_group.ecs_tasks.id,
+  ]
+
+  tags = {
+    Name = "ECR API VPC Endpoint - ${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# Create a VPC Endpoint for CloudWatch
+resource "aws_vpc_endpoint" "cloudwatch" {
+  vpc_id       = "${aws_vpc.main.id}"
+  service_name = "com.amazonaws.${var.region}.logs"
+  vpc_endpoint_type = "Interface"
+  subnet_ids          = aws_subnet.private_subnets.*.id
+  private_dns_enabled = true
+
+  security_group_ids = [
+    aws_security_group.ecs_tasks.id,
+  ]
+
+  tags = {
+    Name = "CloudWatch VPC Endpoint - ${var.environment}"
+    Environment = var.environment
+  }
+}
+
+# Create a VPC Endpoint for S3
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = "${aws_vpc.main.id}"
+  service_name = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [aws_vpc.main.default_route_table_id]
+
+  tags = {
+    Name = "S3 VPC Endpoint Gateway - ${var.environment}"
+    Environment = var.environment
+  }
+}
