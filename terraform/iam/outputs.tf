63a5094c2b52cf3a74d31208b1ce6901f5bfc7d5
mathewsrc <mattheus_ribeiro@outlook.com>
2024-01-23 21:38:19 -0300

    Add ecs_aws_iam_role output

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -5,3 +5,9 @@ output "bedrock_role" {
 output "ecs_task_execution_role_arn" {
   value = aws_iam_role.ecs_task_executor_role.arn
 }
+
+output "ecs_aws_iam_role" {
+  value = {}
+
+  depends_on = [aws_iam_role.ecs_task_executor_role]
+}
