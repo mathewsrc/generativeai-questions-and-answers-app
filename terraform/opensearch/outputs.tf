29a15f7b85e89458e816cc3ee4f8a6a2feb7f6a2
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-05 15:22:50 -0300

    Delete OpenSearch Serverless Terraform files

--- terraform/opensearch/outputs.tf
+++
@@ -1,8 +0,0 @@
-
-output "collection_enpdoint" {
-  value = aws_opensearchserverless_collection.collection.collection_endpoint
-}
-
-output "dashboard_endpoint" {
-  value = aws_opensearchserverless_collection.collection.dashboard_endpoint
-}
