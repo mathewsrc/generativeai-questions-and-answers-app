313d3db8917d6e086e873a3e6f2d118f22352f6c
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-05 15:35:04 -0300

    Remove unnecessary outputs in iam/outputs.tf

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -1,17 +1,3 @@
 output "bedrock_role_arn" {
   value = aws_iam_role.bedrock.arn
 }
-
-output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.ecs_task_executor_role.arn
-}
-
-output "ecs_aws_iam_role" {
-  value = {}
-
-  depends_on = [aws_iam_role.ecs_task_executor_role]
-}
-
-output "ecs_task_role_arn" {
-  value = aws_iam_role.ecs_task_role.arn
-}
