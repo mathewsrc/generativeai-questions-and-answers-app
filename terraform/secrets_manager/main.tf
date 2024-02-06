6f54c8306fde6b2dbf7bee213e5cc529f5ecca4b
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-06 15:55:14 -0300

    Update force_overwrite_replica_secret value in main.tf

--- terraform/secrets_manager/main.tf
+++ terraform/secrets_manager/main.tf
@@ -1,8 +1,8 @@
 resource "aws_secretsmanager_secret" "qdrant_url" {
   name                           = var.qdrant_url_key
   description                    = "Qdrant URL Key"
-  recovery_window_in_days        = 0     # Force deletion without recovery
-  force_overwrite_replica_secret = false # Force overwrite a secret with the same name in the destination Region.
+  recovery_window_in_days        = 0    # Force deletion without recovery
+  force_overwrite_replica_secret = true # Force overwrite a secret with the same name in the destination Region.
   tags = {
     Name        = "Qdrant URL"
     Environment = var.environment
@@ -13,8 +13,8 @@ resource "aws_secretsmanager_secret" "qdrant_url" {
 resource "aws_secretsmanager_secret" "qdrant_api_key" {
   name                           = var.qdrant_api_key
   description                    = "Qdrant API Key"
-  recovery_window_in_days        = 0     # Force deletion without recovery
-  force_overwrite_replica_secret = false # Force overwrite a secret with the same name in the destination Region.
+  recovery_window_in_days        = 0    # Force deletion without recovery
+  force_overwrite_replica_secret = true # Force overwrite a secret with the same name in the destination Region.
   tags = {
     Name        = "Qdrant API Key"
     Environment = var.environment
