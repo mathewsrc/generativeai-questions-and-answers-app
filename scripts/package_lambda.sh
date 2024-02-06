053ad90eed3836b00eadc7946f342bb6e9166959
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-06 15:06:43 -0300

    Update package_lambda.sh script to create a layer zip file and a  payload zip file

--- scripts/package_lambda.sh
+++ scripts/package_lambda.sh
@@ -7,16 +7,16 @@ cd lambda
 mkdir package
 
 # Install dependencies
-pip install --target ./package boto3 langchain-community qdrant-client python-dotenv pydantic
+pip install --target ./package boto3 langchain-community qdrant-client python-dotenv
 
 # Move to the package directory
 cd package
 
-# Create a .zip file with the installed libraries at the root
-zip -r ../../lambda_payload.zip .
+# Create a layer .zip file with the installed libraries at the root
+zip -r ../../lambda_layer.zip .
 
-# Move back to the parent directory
-cd ..
+# Move to the functions directory
+cd ../functions
 
 # Add all *.py files to the root of the .zip file
 zip ../lambda_payload.zip *.py
