b90732c3bec91b15c917900918fe278599463503
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-06 17:15:04 -0300

    Update dependencies in outputs.tf file

--- terraform/lambda/outputs.tf
+++ terraform/lambda/outputs.tf
@@ -1,5 +1,5 @@
 output "wait_for_lambda_deployment" {
   value = {}
-  
-  depends_on = [aws_s3_bucket_notification.bucket_notification.id]
+
+  depends_on = [aws_s3_bucket_notification.bucket_notification]
 }
