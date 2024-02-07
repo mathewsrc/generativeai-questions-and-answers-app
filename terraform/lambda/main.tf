0612f350253c849822045ff93d2c3c59f9cdd714
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-07 14:30:27 -0300

    Add comments

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -1,5 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification
 
+# Execute a Bash script to create the libraries package
 resource "null_resource" "package_lambda" {
   provisioner "local-exec" {
     command     = "chmod +x ${path.module}/../../scripts/package_lambda.sh; ${path.module}/../../scripts/package_lambda.sh"
@@ -7,13 +8,15 @@ resource "null_resource" "package_lambda" {
   }
 }
 
+# Create a zip file from the libraries package
 data "archive_file" "layer" {
   type        = "zip"
   output_path = "${path.module}/../../files/lambda_layer.zip"
-  source_dir  = "${path.module}/../../lambda/python"
+  source_dir  = "${path.module}/temp"
   depends_on  = [null_resource.package_lambda]
 }
 
+# Create a zip file from the lambda functions
 data "archive_file" "lambda" {
   type        = "zip"
   output_path = "${path.module}/../../files/lambda_payload.zip"
@@ -25,7 +28,7 @@ data "archive_file" "lambda" {
   depends_on = [null_resource.package_lambda]
 }
 
-# create a s3 bucket to store the lambda layer
+# Create an S3 bucket to store the lambda layer
 resource "aws_s3_bucket" "layer" {
   bucket        = var.bucket_name
   force_destroy = true
@@ -37,12 +40,11 @@ resource "aws_s3_bucket" "layer" {
   }
 }
 
-# upload the lambda layer to s3
+# Create an S3 object to store the lambda layer
 resource "aws_s3_object" "object" {
-  # Recursively look for pdf files inside documents/ 
   bucket = aws_s3_bucket.layer.id
   key    = "lambda_layer.zip"
-  source = "${path.module}/../../files/lambda_layer.zip"
+  source = data.archive_file.layer.output_path
 
   tags = {
     Name        = "${var.bucket_name} Bucket"
@@ -55,8 +57,8 @@ resource "aws_s3_object" "object" {
   ]
 }
 
+# Create an Lambda layer
 resource "aws_lambda_layer_version" "lambda_layer" {
-  #filename    = "${path.module}/../../files/lambda_layer.zip" # Error: Hit the 50MB limit
   s3_bucket   = aws_s3_bucket.layer.id
   s3_key      = aws_s3_object.object.id
   layer_name  = "lambda_layer"
@@ -65,6 +67,7 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   compatible_runtimes = ["python3.12"]
 }
 
+# Get the Qdrant URL and API key from the environment
 data "external" "envs" {
   program = ["sh", "-c", <<-EOSCRIPT
     jq -n '{ "qdrant_url": $QDRANT_URL, "qdrant_api_key": $QDRANT_API_KEY }' \
@@ -74,6 +77,7 @@ data "external" "envs" {
   ]
 }
 
+# Create a Lambda Function
 resource "aws_lambda_function" "func" {
   depends_on = [
     aws_iam_role_policy_attachment.lambda_policy_attachment,
@@ -82,12 +86,12 @@ resource "aws_lambda_function" "func" {
 
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "${path.module}/../../files/lambda_payload.zip"
+  filename      = data.archive_file.lambda.output_path
   function_name = var.lambda_function_name
   role          = aws_iam_role.lambda_role.arn
-  handler       = "main.py" # Function entrypoint 
+  handler       = "main.lambda_handler" # module main.py and function lambda_handler
   runtime       = "python3.12"
-  memory_size   = 256
+  memory_size   = var.memory_size
 
   layers = [aws_lambda_layer_version.lambda_layer.arn]
 
@@ -107,6 +111,7 @@ resource "aws_lambda_function" "func" {
   }
 }
 
+# Allow the Lambda function to be invoked by the S3 bucket
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
@@ -115,6 +120,7 @@ resource "aws_lambda_permission" "allow_bucket" {
   source_arn    = var.s3_bucket_arn
 }
 
+# Create an S3 bucket notification
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = var.s3_bucket_id
 
