43613d653226b6fbfec596b1d5aaeb8f684745a6
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-07 14:28:47 -0300

    Update package_lambda.sh script to include additional dependencies

--- scripts/package_lambda.sh
+++ scripts/package_lambda.sh
@@ -4,13 +4,21 @@
 cd lambda
 
 # Create a directory for the python [place the libraries in the /python or python/lib/python3.x/site-packages folders]
-mkdir -p python
+mkdir -p temp/python
 
 # Install dependencies
-pip3 install --platform manylinux2014_x86_64 --target ./python --python-version 3.12 --only-binary=:all: boto3 langchain-community qdrant-client python-dotenv
+pip3 install --platform manylinux2014_x86_64 --target ./temp/python\
+ --python-version 3.12 \
+ --only-binary=:all: boto3 \
+    langchain \
+    langchain-community \
+    qdrant-client \
+    python-dotenv \
+    unstructured \
+    pypdf
 
-# Create a layer .zip file with the installed libraries at the root
-zip -r ../lambda_layer.zip ./python
+# Create a layer .zip file with the installed libraries at the root [Not required if you are using Terraform]
+#zip -r ../lambda_layer.zip ./temp/python
 
-# Add all *.py files to the root of the .zip file
-zip ../lambda_payload.zip ./functions/*.py
+# Add all *.py files to the root of the .zip file [Not required if you are using Terraform]
+#zip ../lambda_payload.zip ./functions/*.py
