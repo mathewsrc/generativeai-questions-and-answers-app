a137615628fd77a5d4a0b2f7a0cf14f4be7d6977
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-08 17:35:43 -0300

    Refactor Terraform module imports

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,18 +26,59 @@ provider "aws" {
 #   environment      = var.environment
 # }
 
-module "secrets_manager" {
-  source      = "./secrets_manager"
-  environment = var.environment
-  region      = data.aws_region.current.name
-}
+# module "secrets_manager" {
+#   source      = "./secrets_manager"
+#   environment = var.environment
+#   region      = data.aws_region.current.name
+# }
 
-module "network" {
-  source           = "./network"
-  region           = data.aws_region.current.name
-  application_name = var.name
-  environment      = var.environment
-}
+# module "network" {
+#   source           = "./network"
+#   region           = data.aws_region.current.name
+#   application_name = var.name
+#   environment      = var.environment
+# }
+
+# module "load_balancer" {
+#   source           = "./load_balancer"
+#   region           = data.aws_region.current.name
+#   application_name = var.name
+#   environment      = var.environment
+#   public_subnets   = module.network.public_subnets
+#   vpc_id           = module.network.vpc_id
+#   container_port   = var.container_port
+# }
+
+# module "ecr" {
+#   source           = "./ecr"
+#   region           = data.aws_region.current.name
+#   application_name = var.name
+#   environment      = var.environment
+# }
+
+# module "ecs" {
+#   source                      = "./ecs"
+#   region                      = data.aws_region.current.name
+#   application_name            = var.name
+#   environment                 = var.environment
+#   ecr_repository_url          = module.ecr.ecr_repository_url
+#   ecr_repository_name         = module.ecr.ecr_repository_name
+#   vpc_id                      = module.network.vpc_id
+#   private_subnets             = module.network.private_subnets
+#   nlb_target_group_arn        = module.load_balancer.nlb_target_group_arn
+#   container_port              = var.container_port
+#   ecs_tasks_security_group_id = [module.network.ecs_tasks_security_group_id]
+# }
+
+# module "api_gateway" {
+#   source           = "./api_gateway"
+#   region           = data.aws_region.current.name
+#   application_name = var.name
+#   environment      = var.environment
+#   nlb_dns_name     = module.load_balancer.nlb_dns_name
+#   nlb_arn          = module.load_balancer.nlb_arn
+#   container_port   = var.container_port
+# }
 
 module "s3" {
   source                     = "./s3"
@@ -57,46 +98,7 @@ module "lambda" {
   s3_bucket_arn    = module.s3.s3_bucket_arn
 }
 
-module "load_balancer" {
-  source           = "./load_balancer"
-  region           = data.aws_region.current.name
-  application_name = var.name
-  environment      = var.environment
-  public_subnets   = module.network.public_subnets
-  vpc_id           = module.network.vpc_id
-  container_port   = var.container_port
-}
 
-module "ecr" {
-  source           = "./ecr"
-  region           = data.aws_region.current.name
-  application_name = var.name
-  environment      = var.environment
-}
-
-module "ecs" {
-  source                      = "./ecs"
-  region                      = data.aws_region.current.name
-  application_name            = var.name
-  environment                 = var.environment
-  ecr_repository_url          = module.ecr.ecr_repository_url
-  ecr_repository_name         = module.ecr.ecr_repository_name
-  vpc_id                      = module.network.vpc_id
-  private_subnets             = module.network.private_subnets
-  nlb_target_group_arn        = module.load_balancer.nlb_target_group_arn
-  container_port              = var.container_port
-  ecs_tasks_security_group_id = [module.network.ecs_tasks_security_group_id]
-}
-
-module "api_gateway" {
-  source           = "./api_gateway"
-  region           = data.aws_region.current.name
-  application_name = var.name
-  environment      = var.environment
-  nlb_dns_name     = module.load_balancer.nlb_dns_name
-  nlb_arn          = module.load_balancer.nlb_arn
-  container_port   = var.container_port
-}
 
 # Define the local backend for the Terraform state
 # terraform {
