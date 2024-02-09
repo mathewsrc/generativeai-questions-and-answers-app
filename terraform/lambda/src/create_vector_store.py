5a5f9cc48728c00c63f7ecce0f8af8ce72b3f952
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-09 15:34:03 -0300

    Add create_vector_store.py, utils.py, and main.py

--- terraform/lambda/src/create_vector_store.py
+++ terraform/lambda/src/create_vector_store.py
@@ -3,6 +3,8 @@ from langchain_community.document_loaders import S3FileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient
 import json
+import logging
+import os
 
 from utils import (
 	get_embeddings,
@@ -19,26 +21,28 @@ huggingface_embeddings = [
 
 aws_embeddings = ["amazon.titan-embed-text-v1"]
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#os.chdir("tmp/")
 
 def info(url: str, api_key: str, collection_name: str) -> None:
-	try:
-		client = get_client(url, api_key)
-		info = client.get_collection(collection_name=collection_name)
-		print(json.dumps(f"Collection info\n: {info}", indent=4))
-	except Exception as e:
-		print(f"Error: {e}")
+	client = get_client(url, api_key)
+	info = client.get_collection(collection_name=collection_name)
+	logger.info(json.dumps(f"Collection info\n: {info}", indent=4))
 
 
 def get_documents_from_pdf(
 	bucket_name: str, key: str, collection_name: str, region_name: str
 ) -> list:
+	
 	loader = S3FileLoader(bucket=bucket_name, key=key, region_name=region_name)
 	documents = loader.load()
 
 	# Split documents
 	text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 	docs = text_splitter.split_documents(documents)
-	print(f"Number of documents after split: {len(docs)}")
+	logger.info(f"Number of documents after split: {len(docs)}")
 	return docs
 
 
@@ -55,41 +59,39 @@ def create_vectostore(
 	region_name: str,
 	embedding_model: str,
 ) -> None:
-	try:
-		docs = get_documents_from_pdf(
-			bucket_name=bucket_name,
-			key=object_key,
-			collection_name=collection_name,
-			region_name=region_name,
-		)
+	docs = get_documents_from_pdf(
+		bucket_name=bucket_name,
+		key=object_key,
+		collection_name=collection_name,
+		region_name=region_name,
+	)
 
-		if embedding_model in huggingface_embeddings:
-			embedding = Embeddings.HUGGINGFACE
-			model_name = embedding_model
+	if embedding_model in huggingface_embeddings:
+		embedding = Embeddings.HUGGINGFACE
+		model_name = embedding_model
 
-		elif embedding_model in aws_embeddings:
-			embedding = Embeddings.BEDROCK
-			model_name = embedding_model
+	elif embedding_model in aws_embeddings:
+		embedding = Embeddings.BEDROCK
+		model_name = embedding_model
 
-		else:
-			raise ValueError("Invalid embedding model name")
+	else:
+		raise ValueError("Invalid embedding model name")
 
-		embeddings = get_embeddings(
-			embedding=Embedding(embeddings=embedding, model_name=model_name),
-			region_name=region_name,
-		)
-		print("Creating collection...")
+	embeddings = get_embeddings(
+		embedding=Embedding(embeddings=embedding, model_name=model_name),
+		region_name=region_name,
+	)
+	logger.info("Creating collection...")
 
-		vectorstore = Qdrant.from_documents(
-			documents=docs,
-			embedding=embeddings,
-			url=url,
-			prefer_grpc=True,
-			api_key=api_key,
-			collection_name=collection_name,
-			force_recreate=False,
-		)
-		vectorstore.client.close()
-		info(url, api_key, collection_name)
-	except Exception as e:
-		print(f"Error: {e}")
+	vectorstore = Qdrant.from_documents(
+		documents=docs,
+		embedding=embeddings,
+		url=url,
+		prefer_grpc=True,
+		api_key=api_key,
+		collection_name=collection_name,
+		force_recreate=False,
+	)
+	vectorstore.client.close()
+	info(url, api_key, collection_name)
+	
--- terraform/lambda/src/utils.py
+++ terraform/lambda/src/utils.py
@@ -3,7 +3,10 @@ from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.embeddings import BedrockEmbeddings
 import boto3
 from dataclasses import dataclass
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class Embeddings(Enum):
 	HUGGINGFACE = "HuggingFaceEmbeddings"
@@ -22,20 +25,20 @@ def get_huggingface_embeddings(model_name: str) -> HuggingFaceEmbeddings:
 	embeddings = HuggingFaceEmbeddings(
 		model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
 	)
-	print("Embedding finished!")
+	logger.info("Embedding finished!")
 	return embeddings
 
 
 def get_bedrock_embeddings(model_name: str, region_name: str) -> BedrockEmbeddings:
 	bedrock_runtime = boto3.client("bedrock-runtime", region_name=region_name)
 	embeddings = BedrockEmbeddings(client=bedrock_runtime, model_id=model_name)
-	print("Embedding finished!")
+	logger.info("Embedding finished!")
 	return embeddings
 
 
 def get_embeddings(embedding: Embedding, region_name: str):
-	print(f"Embedding model: {embedding.model_name}")
-	print(f"Embedding type: {embedding.embeddings}")
+	logger.info(f"Embedding model: {embedding.model_name}")
+	logger.info(f"Embedding type: {embedding.embeddings}")
 	if embedding.embeddings == Embeddings.HUGGINGFACE:
 		return get_huggingface_embeddings(embedding.model_name)
 	elif embedding.embeddings == Embeddings.BEDROCK:
