d2af752bb037e37c3c9739c6e3f3c5c94b8cea68
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-09 15:32:58 -0300

    Update deploy_lambda.sh script

--- scripts/deploy_lambda.sh
+++ scripts/deploy_lambda.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e # Exit if any command fails
+
+pwd
 
 # This script is used to deploy the application
 
@@ -7,31 +10,33 @@ AWS_REGION=$(aws configure get region)
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_ECR_REPOSITORY_NAME=lambda-repo # Replace with your ECR repository name
 AWS_ECR_REPOSITORY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPOSITORY_NAME
-TAG=$(git rev-parse HEAD) # Get the short commit hash
+TAG=$(git rev-parse HEAD) # Get current short commit hash
 
 # Login to AWS ECR
+echo "Logging in to AWS ECR..."
 aws ecr get-login-password \
     --region $AWS_REGION | docker login \
     --username AWS \
     --password-stdin $AWS_ECR_REPOSITORY_URL
 
 # Build the Docker image
-docker build -t $AWS_ECR_REPOSITORY_NAME -f lambda/Dockerfile .
+echo "Building Docker image..."
+docker build -t $AWS_ECR_REPOSITORY_NAME -f ./lambda/docker/Dockerfile .
 
 # Check if the ECR repository exists
-aws ecr describe-repositories \
+echo "Checking if ECR repository exists..."
+if aws ecr describe-repositories \
     --repository-names $AWS_ECR_REPOSITORY_NAME \
-    --region $AWS_REGION > /dev/null 2>&1
+    --region $AWS_REGION > /dev/null 2>&1; then
+    echo "ECR repository exists, deleting..."
+    aws ecr delete-repository \
+    --repository-name $AWS_ECR_REPOSITORY_NAME \
+    --region $AWS_REGION \
+    --force
+fi
 
-    # If the repository exists, delete it
-    if [ $? -eq 0 ]; then
-        aws ecr delete-repository \
-            --repository-name $AWS_ECR_REPOSITORY_NAME \
-            --region $AWS_REGION \
-            --force
-    fi
-    
-# Create the ECR repository if it doesn't exist
+# Create the ECR repository
+echo "Creating ECR repository..."
 aws ecr create-repository \
     --repository-name $AWS_ECR_REPOSITORY_NAME \
     --region $AWS_REGION \
@@ -39,9 +44,12 @@ aws ecr create-repository \
     --image-tag-mutability MUTABLE
 
 # Tag the Docker image
+echo "Tagging Docker image..."
 docker tag $AWS_ECR_REPOSITORY_NAME:latest $AWS_ECR_REPOSITORY_URL:$TAG
 
 # Push the Docker image
+echo "Pushing Docker image..."
 docker push $AWS_ECR_REPOSITORY_URL:$TAG
 
-export LAMBDA_ECR_REPOSITORY_URL=$AWS_ECR_REPOSITORY_URL:$TAG
+echo "export LAMBDA_ECR_REPOSITORY_URL=$AWS_ECR_REPOSITORY_URL:$TAG" >> ~/.bashrc
+source ~/.bashrc
