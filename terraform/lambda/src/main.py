a0d5169659a2e7faddb14ad7479dcc43defa0e19
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-09 15:40:24 -0300

    Add NLTK download for punkt tokenizer

--- terraform/lambda/src/main.py
+++ terraform/lambda/src/main.py
@@ -2,6 +2,8 @@ import json
 from create_vector_store import create_vectostore
 import os
 from dotenv import load_dotenv
+import nltk
+import os
 
 load_dotenv()
 
@@ -11,11 +13,14 @@ AWS_REGION = "us-east-1"
 BUCKET_NAME = "bedrock-question-answer"
 
 QDRANT_URL = os.environ.get("QDRANT_URL")
-QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
+QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")]
+NLTK_DATA = os.environ.get("NLTK_DATA")
 
 def lambda_handler(event, context):
 	bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
 	object_key = event["Records"][0]["s3"]["object"]["key"]
+
+	nltk.download('punkt', download_dir=NLTK_DATA)
 
 	create_vectostore(
 		url=QDRANT_URL,
