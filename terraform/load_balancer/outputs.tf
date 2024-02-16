ab06ba1cccac8f0923dba5841b68e0669a8be32e
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-16 14:10:01 -0300

    Update load balancer output names

--- terraform/load_balancer/outputs.tf
+++ terraform/load_balancer/outputs.tf
@@ -1,11 +1,11 @@
-output "nlb_target_group_arn" {
+output "lb_target_group_arn" {
   value = aws_lb_target_group.target_group.arn
 }
 
-output "nlb_dns_name" {
-  value = aws_lb.nlb.dns_name
+output "lb_dns_name" {
+  value = aws_lb.lb.dns_name
 }
 
-output "nlb_arn" {
-  value = aws_lb.nlb.arn
+output "lb_arn" {
+  value = aws_lb.lb.arn
 }
