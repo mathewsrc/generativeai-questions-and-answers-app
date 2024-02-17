db5a606f776ed00fe232d4b0cab447ea552ff89f
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-17 14:41:24 -0300

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for DIR in */; do
-    DIRNAME=$(basename "$DIR")
-    echo "==> $DIRNAME <=="
-    (cd $DIR && poetry run python -m pytest -vv --cov=test/*.py)
-done
-
-echo "Format complete."
