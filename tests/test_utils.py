7d404b825f96f3c4150ecd916b3564d9b7e536be
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-19 11:35:29 -0300

    Refactor test_utils.py to improve code readability

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,20 +2,20 @@ import pytest
 from terraform.lambda_functions.src.utils import get_embeddings, Embedding, Embeddings
 from langchain_community.embeddings import BedrockEmbeddings
 
+
 def test_embedding_invalid():
-    with pytest.raises(ValueError, match="'INVALID' is not a valid Embeddings"):
-        Embedding(Embeddings("INVALID"), "invalid_model")
-        
+	with pytest.raises(ValueError, match="'INVALID' is not a valid Embeddings"):
+		Embedding(Embeddings("INVALID"), "invalid_model")
+
+
 def test_embedding_valid():
-    embedding = Embedding(Embeddings.BEDROCK, "amazon.titan-embed-text-v1")
-    assert embedding.model_name == "amazon.titan-embed-text-v1"
-    assert embedding.embeddings == Embeddings.BEDROCK
+	embedding = Embedding(Embeddings.BEDROCK, "amazon.titan-embed-text-v1")
+	assert embedding.model_name == "amazon.titan-embed-text-v1"
+	assert embedding.embeddings == Embeddings.BEDROCK
+
 
 def test_get_embeddings_bedrock():
-    embedding = Embedding(Embeddings.BEDROCK, "amazon.titan-embed-text-v1")
-    result = get_embeddings(embedding, "us-east-1")
-    assert isinstance(result, BedrockEmbeddings)
-    assert result.model_id == "amazon.titan-embed-text-v1"
-
-
-   
+	embedding = Embedding(Embeddings.BEDROCK, "amazon.titan-embed-text-v1")
+	result = get_embeddings(embedding, "us-east-1")
+	assert isinstance(result, BedrockEmbeddings)
+	assert result.model_id == "amazon.titan-embed-text-v1"
