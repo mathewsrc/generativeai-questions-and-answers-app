6da25d4276e77b60731cde27548fb62cb23e6af1
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-19 18:27:04 -0300

    Update docker_run.sh script to prompt user for environment variables

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-echo "Enter AWS_ACCESS_KEY_ID:"
-read AWS_ACCESS_KEY_ID
-echo "Enter AWS_SECRET_ACCESS_KEY:"
-read AWS_SECRET_ACCESS_KEY
-echo "Enter AWS_DEFAULT_REGION:"
-read AWS_DEFAULT_REGION
+# check if Environment variables are not set, ask user to enter them
+if [ -z "$QDRANT_URL" ]; then
+	echo "Please enter QDRANT_URL"
+	read QDRANT_URL
+fi
 
-docker run -p 80:80 --env-file .env \
+if [ -z "$QDRANT_API_KEY" ]; then
+	echo "Please enter QDRANT_API_KEY"
+	read QDRANT_API_KEY
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+	echo "Please enter AWS_ACCESS_KEY_ID"
+	read AWS_ACCESS_KEY_ID
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+	echo "Please enter AWS_SECRET_ACCESS_KEY"
+	read AWS_SECRET_ACCESS_KEY
+fi
+
+docker run -p 80:80 \
 	-e QDRANT_URL=$QDRANT_URL \
 	-e QDRANT_API_KEY=$QDRANT_API_KEY \
 	-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
