fc2f68a8b7669e12ca5acd317545d85f566a5979
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-19 11:35:05 -0300

    Remove unused HuggingFaceEmbeddings import and function

--- terraform/lambda_functions/src/utils.py
+++ terraform/lambda_functions/src/utils.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.embeddings import BedrockEmbeddings
 import boto3
 from dataclasses import dataclass
@@ -8,8 +7,8 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 class Embeddings(Enum):
-	HUGGINGFACE = "HuggingFaceEmbeddings"
 	BEDROCK = "BedrockEmbeddings"
 
 
@@ -17,16 +16,6 @@ class Embeddings(Enum):
 class Embedding:
 	embeddings: Embeddings
 	model_name: str
-
-
-def get_huggingface_embeddings(model_name: str) -> HuggingFaceEmbeddings:
-	model_kwargs = {"device": "cpu"}
-	encode_kwargs = {"normalize_embeddings": False}
-	embeddings = HuggingFaceEmbeddings(
-		model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
-	)
-	logger.info("Embedding finished!")
-	return embeddings
 
 
 def get_bedrock_embeddings(model_name: str, region_name: str) -> BedrockEmbeddings:
@@ -39,9 +28,7 @@ def get_bedrock_embeddings(model_name: str, region_name: str) -> BedrockEmbeddin
 def get_embeddings(embedding: Embedding, region_name: str):
 	logger.info(f"Embedding model: {embedding.model_name}")
 	logger.info(f"Embedding type: {embedding.embeddings}")
-	if embedding.embeddings == Embeddings.HUGGINGFACE:
-		return get_huggingface_embeddings(embedding.model_name)
-	elif embedding.embeddings == Embeddings.BEDROCK:
+	if embedding.embeddings == Embeddings.BEDROCK:
 		return get_bedrock_embeddings(embedding.model_name, region_name=region_name)
 	else:
 		raise Exception("Invalid embedding type")
