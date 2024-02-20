b146ec203d82adf1804210dbdc0c11ccd4ca7393
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-20 16:09:10 -0300

    Add load balancer security group output

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -2,6 +2,10 @@ output "ecs_tasks_security_group_id" {
   value = aws_security_group.ecs_tasks.id
 }
 
+output "load_balancer_security_group_id" {
+  value = aws_security_group.lb.id
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
