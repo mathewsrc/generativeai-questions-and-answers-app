cbee9086b549c91faaac3b3c4c5abff7e7f5b51c
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-20 16:09:02 -0300

    Update load balancer configuration

--- terraform/load_balancer/main.tf
+++ terraform/load_balancer/main.tf
@@ -2,7 +2,7 @@
 resource "aws_lb" "lb" {
   name                       = var.nlb_name
   internal                   = true
-  load_balancer_type         = "network"
+  load_balancer_type         = "application"
   subnets                    = var.public_subnets
   enable_deletion_protection = false
 
@@ -19,7 +19,7 @@ resource "aws_lb_target_group" "target_group" {
   depends_on  = [aws_lb.lb]
   name        = var.target_group_name
   port        = var.container_port
-  protocol    = "TCP"
+  protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
 }
@@ -28,7 +28,7 @@ resource "aws_lb_target_group" "target_group" {
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.lb.arn
   port              = var.container_port
-  protocol          = "TCP"
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
