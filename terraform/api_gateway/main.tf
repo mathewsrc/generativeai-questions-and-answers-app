55ab8ce4cf8dbb45986b42ab357ed7e70cf5e9bc
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-20 19:21:34 -0300

    Refactor API Gateway integrations and stage configuration

--- terraform/api_gateway/main.tf
+++ terraform/api_gateway/main.tf
@@ -17,23 +17,21 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
 }
 
 resource "aws_apigatewayv2_integration" "root_integration" {
-  api_id               = aws_apigatewayv2_api.example.id
-  integration_type     = "HTTP_PROXY"
-  integration_uri      = var.lb_listener_arn
-  integration_method   = "GET"
-  connection_type      = "VPC_LINK"
-  connection_id        = aws_apigatewayv2_vpc_link.vpc_link.id
-  timeout_milliseconds = var.api_timeout_milliseconds
+  api_id             = aws_apigatewayv2_api.example.id
+  integration_type   = "HTTP_PROXY"
+  integration_uri    = var.lb_listener_arn
+  integration_method = "GET"
+  connection_type    = "VPC_LINK"
+  connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
 }
 
 resource "aws_apigatewayv2_integration" "ask_integration" {
-  api_id               = aws_apigatewayv2_api.example.id
-  integration_type     = "HTTP_PROXY"
-  integration_uri      = var.lb_listener_arn
-  integration_method   = "POST"
-  connection_type      = "VPC_LINK"
-  connection_id        = aws_apigatewayv2_vpc_link.vpc_link.id
-  timeout_milliseconds = var.api_timeout_milliseconds
+  api_id             = aws_apigatewayv2_api.example.id
+  integration_type   = "HTTP_PROXY"
+  integration_uri    = var.lb_listener_arn
+  integration_method = "POST"
+  connection_type    = "VPC_LINK"
+  connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
 }
 
 resource "aws_apigatewayv2_route" "root_route" {
@@ -48,43 +46,11 @@ resource "aws_apigatewayv2_route" "ask_route" {
   target    = "integrations/${aws_apigatewayv2_integration.ask_integration.id}"
 }
 
-resource "aws_apigatewayv2_deployment" "example" {
-  api_id      = aws_apigatewayv2_api.example.id
-  description = "Example deployment"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_apigatewayv2_route.root_route,
-    aws_apigatewayv2_route.ask_route,
-    aws_apigatewayv2_integration.root_integration,
-    aws_apigatewayv2_integration.ask_integration
-  ]
-}
-
 resource "aws_apigatewayv2_stage" "example" {
-  depends_on = [
-    aws_apigatewayv2_deployment.example
-  ]
-  api_id        = aws_apigatewayv2_api.example.id
-  description   = "Example stage"
-  name          = "example-stage"
-  deployment_id = aws_apigatewayv2_deployment.example.id
-  auto_deploy   = false # Whether updates to an API automatically trigger a new deployment.
-
-  route_settings {
-    route_key              = aws_apigatewayv2_route.ask_route.route_key
-    throttling_burst_limit = var.throttling_burst_limit
-    throttling_rate_limit  = var.throttling_rate_limit
-  }
-
-  default_route_settings {
-    detailed_metrics_enabled = true
-    logging_level            = "INFO"
-  }
-
+  api_id      = aws_apigatewayv2_api.example.id
+  description = "Stage for HTTP API"
+  name        = "$default" # The $default stage is a special stage that's automatically associated with new deployments.
+  auto_deploy = true       # Whether updates to an API automatically trigger a new deployment.
   tags = {
     Environment = var.environment
     Application = var.application_name
