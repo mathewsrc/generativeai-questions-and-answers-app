95e260fcddd8478e17e5e3ae7f64d951a0e56dc6
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-21 20:40:29 -0300

    Update secrets in set_secrets.sh script

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -1,7 +1,7 @@
 
 echo "Enter QDRANT_URL:"
-read QDRANT_URL_AWS
+read QDRANT_URL
 echo "Enter QDRANT_API_KEY:"
-read QDRANT_API_KEY_AWS
-aws secretsmanager put-secret-value --secret-id prod/qdrant_url --secret-string $QDRANT_URL_AWS 
-aws secretsmanager put-secret-value --secret-id prod/qdrant_api_key --secret-string $QDRANT_API_KEY_AWS
+read QDRANT_API_KEY
+aws secretsmanager put-secret-value --secret-id prod/qdrant_url --secret-string $QDRANT_URL 
+aws secretsmanager put-secret-value --secret-id prod/qdrant_api_key --secret-string $QDRANT_API_KEY
