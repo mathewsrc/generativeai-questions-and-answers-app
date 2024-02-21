a9112136dc114b05e0462b676762267f6ab2d462
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-21 20:40:35 -0300

    Update Qdrant environment variables in main.tf

--- terraform/lambda_functions/main.tf
+++ terraform/lambda_functions/main.tf
@@ -21,13 +21,13 @@ resource "null_resource" "deploy_lambda_container" {
 # Get the Qdrant URL and API key from the environment
 data "external" "envs" {
   program = ["bash", "-c", <<-EOSCRIPT
-    : "$${QDRANT_URL_AWS:?Missing environment variable QDRANT_URL_AWS}"
-    : "$${QDRANT_API_KEY_AWS:?Missing environment variable QDRANT_API_KEY_AWS}"
-    jq --arg QDRANT_URL_AWS "$(printenv QDRANT_URL_AWS)" \
-       --arg QDRANT_API_KEY_AWS "$(printenv QDRANT_API_KEY_AWS)" \
+    : "$${QDRANT_URL:?Missing environment variable QDRANT_URL}"
+    : "$${QDRANT_API_KEY:?Missing environment variable QDRANT_API_KEY}"
+    jq --arg QDRANT_URL "$(printenv QDRANT_URL)" \
+       --arg QDRANT_API_KEY "$(printenv QDRANT_API_KEY)" \
        --arg SHA "$(git rev-parse HEAD)" \
-       -n '{ "qdrant_url": $QDRANT_URL_AWS, 
-             "qdrant_api_key": $QDRANT_API_KEY_AWS,
+       -n '{ "qdrant_url": $QDRANT_URL, 
+             "qdrant_api_key": $QDRANT_API_KEY,
              "sha": $SHA}'
   EOSCRIPT
   ]
