79e95870be1502cd14b8f237a8b152335148659a
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-21 20:16:20 -0300

    Update set_secrets.sh script to use Qdrant AWSenvironment variables

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -1,7 +1,7 @@
 
 echo "Enter QDRANT_URL:"
-read QDRANT_URL
+read QDRANT_URL_AWS
 echo "Enter QDRANT_API_KEY:"
-read QDRANT_API_KEY
-aws secretsmanager put-secret-value --secret-id prod/qdrant_url --secret-string $QDRANT_URL 
-aws secretsmanager put-secret-value --secret-id prod/qdrant_api_key --secret-string $QDRANT_API_KEY
+read QDRANT_API_KEY_AWS
+aws secretsmanager put-secret-value --secret-id prod/qdrant_url --secret-string $QDRANT_URL_AWS 
+aws secretsmanager put-secret-value --secret-id prod/qdrant_api_key --secret-string $QDRANT_API_KEY_AWS
