c14a43667e999b4a5a83e0317f622c2ef57675ff
mathewsrc <mattheus_ribeiro@outlook.com>
2024-02-23 11:46:35 -0300

    Add ECR repositories for ECS and Lambda

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -4,9 +4,9 @@ data "aws_caller_identity" "current" {}
 # Get the current AWS region
 data "aws_region" "current" {}
 
-# Create an ECR repository
+# Create an ECR repository for ECS
 resource "aws_ecr_repository" "ecr_repo" {
-  name                 = var.ecr_name
+  name                 = var.ecr_name_ecs
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -18,6 +18,24 @@ resource "aws_ecr_repository" "ecr_repo" {
   tags = {
     Environment = var.environment
     Application = var.application_name
-    Name        = var.ecr_name
+    Name        = var.ecr_name_ecs
+  }
+}
+
+# Create an ECR repository for Lambda
+resource "aws_ecr_repository" "lambda_repo" {
+  name                 = var.ecr_name_lambda
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  force_delete = false
+
+  tags = {
+    Environment = var.environment
+    Application = var.application_name
+    Name        = var.ecr_name_lambda
   }
 }
